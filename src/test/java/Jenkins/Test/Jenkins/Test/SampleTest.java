61d02ba3e7a4d677ed1b010db1496282c742569c
mahesh chaudhar <mchaudha@jcp.com>
2016-11-16 14:15:13 +0530

    Code commit

--- src/test/java/Jenkins/Test/Jenkins/Test/SampleTest.java
+++ src/test/java/Jenkins/Test/Jenkins/Test/SampleTest.java
@@ -2,14 +2,12 @@ package Jenkins.Test.Jenkins.Test;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
 
 public class SampleTest {
 
 	@Test
 	public void sampleTest() {
 		System.out.println("Test 12345dfdf");
-		Assert.assertTrue(false);
 	}
 
 }
